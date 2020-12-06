7203f9c007f8a1d21bd0d7929d079380c828d584
Andrey Klemenko <web.klemenko@gmail.com>
2020-12-06 23:36:44 +0200

    complete tasks 4,5,6

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,30 @@
 // Напиши функцию formatString(string) которая принимает строку и форматирует ее если необходимо.
-
 // Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
 // Если длина больше 40 символов, то функция обрезает строку до 40 - ка символов и добавляет
 // в конец строки троеточие '...', после чего возвращает укороченную версию.
 
-// const formatString = function (string) {
-//   const words = string.length('');
+const formatString = function (string) {
+  const lettersInString = string.split('').length;
+  let trimmedString;
 
-//   for (let i = 0; i > 40; i++) {}
-// if (words <= 40) {
-//     return formatString;
-// } else {
+  if (lettersInString > 40) {
+    trimmedString = string.slice(0, 40) + '...';
+    console.log(trimmedString);
+    return;
+  }
+  console.log(string);
+};
 
-// }
-// };
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// вернется форматированная строка
