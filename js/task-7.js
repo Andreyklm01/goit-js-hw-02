d14c5277532ef3788933073521f84678ff48c564
Andrey Klemenko <web.klemenko@gmail.com>
2020-12-08 21:32:45 +0200

    complete task-7

--- js/task-7.js
+++ js/task-7.js
@@ -1,1 +1,32 @@
-// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function (login) {
+  if (login.length >= 4 && login.length < 16) {
+    return login;
+  }
+};
+
+const isLoginUnique = function (allLogins, login) {
+  if (allLogins.includes(login) === false) {
+    return allLogins;
+  }
+};
+
+const addLogin = function (allLogins, login) {
+  if (!isLoginValid(login)) {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  }
+  if (!isLoginUnique(allLogins, login)) {
+    return 'Такой логин уже используется';
+  }
+  if (isLoginUnique && isLoginValid) {
+    allLogins.push(login);
+    return 'Логин успешно добавлен!';
+  }
+};
+
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'test'));
